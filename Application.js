9354941967bce87465d79e025210c3b2c537ad4f
Shubham0277 <141708942+Shubham0277@users.noreply.github.com>
2023-09-01 08:33:57 +0530

    Update Application.js

--- Application.js
+++ Application.js
@@ -1,47 +1,108 @@
-// app.js
+//integrating and triggering functions
 window.addEventListener('load', async () => {
-    if (window.ethereum) {
+    if (window.ethereum) 
+    {
       window.web3 = new Web3(window.ethereum);
       await window.ethereum.enable();
     }
-    else if (window.web3) {
+    else if (window.web3) 
+    {
       window.web3 = new Web3(window.web3.currentProvider);
     }
-    else {
+    else 
+    {
       console.log('Non-Ethereum');
     }
     const contractABI = [
-      {
-        "constant": false,
-        "inputs": [],
-        "name": "additionCryptocurrency",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "receiveCryptocurrency",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-      }
+      [
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "value",
+              "type": "uint256"
+            }
+          ],
+          "name": "decrementBalance",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "value",
+              "type": "uint256"
+            }
+          ],
+          "name": "incrementBalance",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "from",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "value",
+              "type": "uint256"
+            }
+          ],
+          "name": "Transfer",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            }
+          ],
+          "name": "getBalance",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ]
     ];
+  
     const contractAddress = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4'; 
+    //address here is used of online REMIX IDE
+  
     const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
-    window.additionCryptocurrency = async () => {
-      await contractInstance.methods.additionCryptocurrency().send({ from: web3.eth.defaultAccount });
+  
+    window.incrementBalance = async () => {
+      await contractInstance.methods.incrementBalance().send({ from: web3.eth.defaultAccount });
     };
-    window.receiveCryptocurrency = async () => {
-      const value = await contractInstance.methods.receiveCryptocurrency().call();
-      document.receiveElementById('currentValue').innerText = value;
+  
+    window.decrementBalance = async () => {
+      await contractInstance.methods.decrementBalance().send({ from: web3.eth.defaultAccount });
+    };
+  
+    window.getBalance = async () => {
+      const value = await contractInstance.methods.getBalance().call();
+      document.getElementById('currentValue').innerText = value;
     };
   });
