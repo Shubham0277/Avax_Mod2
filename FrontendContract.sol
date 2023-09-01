6524ddffc86b392d041deca54f6e084dabb2377b
Shubham0277 <141708942+Shubham0277@users.noreply.github.com>
2023-09-01 08:34:50 +0530

    Update FrontendContract.sol

--- FrontendContract.sol
+++ FrontendContract.sol
@@ -1,15 +1,23 @@
-// SPDX-License-Identifier: MIT
-// MyContract.sol
+// SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
-contract MyContract {
-  uint public cryptocurrency;
+contract Cryptocurrency {
+    mapping(address => uint256) private balances;
 
-  function additionCryptocurrency() public {
-    cryptocurrency += 1;
-  }
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-  function receiveCryptocurrency() public view returns (uint) {
-    return cryptocurrency;
-  }
+    function incrementBalance(uint256 value) public {
+        balances[msg.sender] += value;
+        emit Transfer(address(0), msg.sender, value);
+    }
+
+    function decrementBalance(uint256 value) public {
+        require(balances[msg.sender] >= value, "Insufficient balance");
+        balances[msg.sender] -= value;
+        emit Transfer(msg.sender, address(0), value);
+    }
+
+    function getBalance(address account) public view returns (uint256) {
+        return balances[account];
+      }
 }
